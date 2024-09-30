29e50f4b9553d993b96ee125b80984084d605cae
Libvulkan <161806671+Libvulkan@users.noreply.github.com>
2024-10-01 00:57:19 +0530

    Update create_ubuntu_bootstraps.sh

--- create_ubuntu_bootstraps.sh
+++ create_ubuntu_bootstraps.sh
@@ -8,107 +8,102 @@
 ## About 5.5 GB of free space is required
 ## And additional 2.5 GB is required for Wine compilation
 
-if [ "$EUID" != 0 ]; then
-	echo "This script requires root rights!"
-	exit 1
+# Ensure the script is run with root privileges
+if [ "$EUID" -ne 0 ]; then
+    echo "This script requires root rights!"
+    exit 1
 fi
 
-if ! command -v debootstrap 1>/dev/null || ! command -v perl 1>/dev/null; then
-	echo "Please install debootstrap and perl and run the script again"
-	exit 1
+# Check for required tools
+if ! command -v debootstrap >/dev/null || ! command -v perl >/dev/null; then
+    echo "Please install debootstrap and perl and run the script again."
+    exit 1
 fi
 
-# Keep in mind that although you can choose any version of Ubuntu/Debian
-# here, but this script has only been tested with Ubuntu 18.04 Bionic
-export CHROOT_DISTRO="bionic"
+# Define variables
+CHROOT_DISTRO="bionic"
+CHROOT_MIRROR="https://ftp.uni-stuttgart.de/ubuntu/"
+MAINDIR="/opt/chroot/"
+CHROOT_X32="${MAINDIR}/${CHROOT_DISTRO}32_chroot"
+CHROOT_X64="${MAINDIR}/${CHROOT_DISTRO}64_chroot"
 
-export CHROOT_MIRROR="https://ftp.uni-stuttgart.de/ubuntu/"
-
-# Set your preferred path for storing chroots
-# Also don't forget to change the path to the chroots in the build_wine.sh
-# script, if you are going to use it
-export MAINDIR=/opt/chroot/
-export CHROOT_X32="${MAINDIR}"/${CHROOT_DISTRO}32_chroot
-export CHROOT_X64="${MAINDIR}"/${CHROOT_DISTRO}64_chroot
-
+# Function to prepare chroot
 prepare_chroot () {
-	if [ "$1" = "32" ]; then
-		CHROOT_PATH="${CHROOT_X32}"
-	else
-		CHROOT_PATH="${CHROOT_X64}"
-	fi
+    local arch=$1
+    local chroot_path="${MAINDIR}/${CHROOT_DISTRO}${arch}_chroot"
 
-	echo "Unmount chroot directories. Just in case."
-	umount -Rl "${CHROOT_PATH}"
+    echo "Unmounting any chroot directories (if mounted)..."
+    umount -Rl "${chroot_path}" 2>/dev/null
 
-	echo "Mount directories for chroot"
-	mount --bind "${CHROOT_PATH}" "${CHROOT_PATH}"
-	mount -t proc /proc "${CHROOT_PATH}"/proc
-	mount --bind /sys "${CHROOT_PATH}"/sys
-	mount --make-rslave "${CHROOT_PATH}"/sys
-	mount --bind /dev "${CHROOT_PATH}"/dev
-	mount --bind /dev/pts "${CHROOT_PATH}"/dev/pts
-	mount --bind /dev/shm "${CHROOT_PATH}"/dev/shm
-	mount --make-rslave "${CHROOT_PATH}"/dev
+    echo "Mounting necessary directories for chroot..."
+    mount --bind "${chroot_path}" "${chroot_path}"
+    mount -t proc /proc "${chroot_path}/proc"
+    mount --bind /sys "${chroot_path}/sys"
+    mount --make-rslave "${chroot_path}/sys"
+    mount --bind /dev "${chroot_path}/dev"
+    mount --bind /dev/pts "${chroot_path}/dev/pts"
+    mount --bind /dev/shm "${chroot_path}/dev/shm"
+    mount --make-rslave "${chroot_path}/dev"
 
-	rm -f "${CHROOT_PATH}"/etc/resolv.conf
-	cp /etc/resolv.conf "${CHROOT_PATH}"/etc/resolv.conf
+    echo "Setting up DNS resolution for chroot..."
+    rm -f "${chroot_path}/etc/resolv.conf"
+    cp /etc/resolv.conf "${chroot_path}/etc/resolv.conf"
 
-	echo "Chrooting into ${CHROOT_PATH}"
-	chroot "${CHROOT_PATH}" /usr/bin/env LANG=en_US.UTF-8 TERM=xterm PATH="/bin:/sbin:/usr/bin:/usr/sbin" /opt/prepare_chroot.sh
+    echo "Entering the chroot environment..."
+    chroot "${chroot_path}" /usr/bin/env LANG=en_US.UTF-8 TERM=xterm \
+        PATH="/bin:/sbin:/usr/bin:/usr/sbin" /opt/prepare_chroot.sh
 
-	echo "Unmount chroot directories"
-	umount -l "${CHROOT_PATH}"
-	umount "${CHROOT_PATH}"/proc
-	umount "${CHROOT_PATH}"/sys
-	umount "${CHROOT_PATH}"/dev/pts
-	umount "${CHROOT_PATH}"/dev/shm
-	umount "${CHROOT_PATH}"/dev
+    echo "Unmounting chroot directories..."
+    umount -l "${chroot_path}"
+    umount "${chroot_path}/proc"
+    umount "${chroot_path}/sys"
+    umount "${chroot_path}/dev/pts"
+    umount "${chroot_path}/dev/shm"
+    umount "${chroot_path}/dev"
 }
 
+# Function to create the build scripts inside chroot
 create_build_scripts () {
-	sdl2_version="2.26.4"
-	faudio_version="23.03"
-	vulkan_headers_version="1.3.239"
-	vulkan_loader_version="1.3.239"
-	spirv_headers_version="sdk-1.3.239.0"
- 	libpcap_version="1.10.4"
-  	libxkbcommon_version="1.6.0"
+    sdl2_version="2.26.4"
+    faudio_version="23.03"
+    vulkan_headers_version="1.3.239"
+    vulkan_loader_version="1.3.239"
+    spirv_headers_version="sdk-1.3.239.0"
+    libpcap_version="1.10.4"
+    libxkbcommon_version="1.6.0"
 
-	cat <<EOF > "${MAINDIR}"/prepare_chroot.sh
+    cat <<EOF > "${MAINDIR}/prepare_chroot.sh"
 #!/bin/bash
 
 apt-get update
-apt-get -y install nano
-apt-get -y install locales
-echo ru_RU.UTF_8 UTF-8 >> /etc/locale.gen
-echo en_US.UTF_8 UTF-8 >> /etc/locale.gen
+apt-get -y install nano locales
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
-echo deb '${CHROOT_MIRROR}' ${CHROOT_DISTRO} main universe > /etc/apt/sources.list
-echo deb '${CHROOT_MIRROR}' ${CHROOT_DISTRO}-updates main universe >> /etc/apt/sources.list
-echo deb '${CHROOT_MIRROR}' ${CHROOT_DISTRO}-security main universe >> /etc/apt/sources.list
-echo deb-src '${CHROOT_MIRROR}' ${CHROOT_DISTRO} main universe >> /etc/apt/sources.list
-echo deb-src '${CHROOT_MIRROR}' ${CHROOT_DISTRO}-updates main universe >> /etc/apt/sources.list
-echo deb-src '${CHROOT_MIRROR}' ${CHROOT_DISTRO}-security main universe >> /etc/apt/sources.list
+echo "deb '${CHROOT_MIRROR}' ${CHROOT_DISTRO} main universe" > /etc/apt/sources.list
+echo "deb '${CHROOT_MIRROR}' ${CHROOT_DISTRO}-updates main universe" >> /etc/apt/sources.list
+echo "deb '${CHROOT_MIRROR}' ${CHROOT_DISTRO}-security main universe" >> /etc/apt/sources.list
+echo "deb-src '${CHROOT_MIRROR}' ${CHROOT_DISTRO} main universe" >> /etc/apt/sources.list
+echo "deb-src '${CHROOT_MIRROR}' ${CHROOT_DISTRO}-updates main universe" >> /etc/apt/sources.list
+echo "deb-src '${CHROOT_MIRROR}' ${CHROOT_DISTRO}-security main universe" >> /etc/apt/sources.list
 apt-get update
 apt-get -y upgrade
 apt-get -y dist-upgrade
-apt-get -y install software-properties-common
+apt-get -y install software-properties-common ccache cmake perl bison gcc-9 g++-9 wget git gcc-mingw-w64 g++-mingw-w64
+
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 add-apt-repository -y ppa:cybermax-dexter/mingw-w64-backport
+
 apt-get update
 apt-get -y build-dep wine-development libsdl2 libvulkan1
-apt-get -y install ccache cmake perl bison gcc-9 g++-9 wget git gcc-mingw-w64 g++-mingw-w64
-apt-get -y install libxpresent-dev libjxr-dev libusb-1.0-0-dev libgcrypt20-dev libpulse-dev libudev-dev libsane-dev libv4l-dev libkrb5-dev libgphoto2-dev liblcms2-dev libcapi20-dev
-apt-get -y install libjpeg62-dev samba-dev
-apt-get -y install libpcsclite-dev libcups2-dev
-apt-get -y install python3.7 python3-pip libxcb-xkb-dev
-apt-get -y clean
-apt-get -y autoclean
+apt-get -y install libxpresent-dev libusb-1.0-0-dev libgcrypt20-dev libpulse-dev \
+libudev-dev libsane-dev libv4l-dev libkrb5-dev libgphoto2-dev liblcms2-dev \
+libcapi20-dev libjpeg62-dev samba-dev libpcsclite-dev libcups2-dev python3-pip \
+libxcb-xkb-dev
+
 pip3 install --upgrade meson ninja
-export PATH="/usr/local/bin:${PATH}"
-mkdir /opt/build_libs
-cd /opt/build_libs
+
+# Compile SDL2, FAudio, Vulkan, etc.
+mkdir -p /opt/build_libs && cd /opt/build_libs
 wget -O sdl.tar.gz https://www.libsdl.org/release/SDL2-${sdl2_version}.tar.gz
 wget -O faudio.tar.gz https://github.com/FNA-XNA/FAudio/archive/${faudio_version}.tar.gz
 wget -O vulkan-loader.tar.gz https://github.com/KhronosGroup/Vulkan-Loader/archive/v${vulkan_loader_version}.tar.gz
@@ -116,60 +111,33 @@ wget -O vulkan-headers.tar.gz https://github.com/KhronosGroup/Vulkan-Headers/arc
 wget -O spirv-headers.tar.gz https://github.com/KhronosGroup/SPIRV-Headers/archive/${spirv_headers_version}.tar.gz
 wget -O libpcap.tar.gz https://www.tcpdump.org/release/libpcap-${libpcap_version}.tar.gz
 wget -O libxkbcommon.tar.xz https://xkbcommon.org/download/libxkbcommon-${libxkbcommon_version}.tar.xz
-if [ -d /usr/lib/i386-linux-gnu ]; then wget -O wine.deb https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-stable_4.0.3~bionic_i386.deb; fi
-if [ -d /usr/lib/x86_64-linux-gnu ]; then wget -O wine.deb https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-amd64/wine-stable_4.0.3~bionic_amd64.deb; fi
-git clone https://gitlab.winehq.org/wine/vkd3d.git
-tar xf sdl.tar.gz
-tar xf faudio.tar.gz
-tar xf vulkan-loader.tar.gz
-tar xf vulkan-headers.tar.gz
-tar xf spirv-headers.tar.gz
-tar xf libpcap.tar.gz
-tar xf libxkbcommon.tar.xz
-export CFLAGS="-O2"
-export CXXFLAGS="-O2"
-mkdir build && cd build
-cmake ../SDL2-${sdl2_version} && make -j$(nproc) && make install
-cd ../ && rm -r build && mkdir build && cd build
-cmake ../FAudio-${faudio_version} && make -j$(nproc) && make install
-cd ../ && rm -r build && mkdir build && cd build
-cmake ../Vulkan-Headers-${vulkan_headers_version} && make -j$(nproc) && make install
-cd ../ && rm -r build && mkdir build && cd build
-cmake ../Vulkan-Loader-${vulkan_loader_version}
-make -j$(nproc)
-make install
-cd ../ && rm -r build && mkdir build && cd build
-cmake ../SPIRV-Headers-${spirv_headers_version} && make -j$(nproc) && make install
-cd ../ && dpkg -x wine.deb .
-cp opt/wine-stable/bin/widl /usr/bin
-cd vkd3d && ./autogen.sh
-cd ../ && rm -r build && mkdir build && cd build
-../vkd3d/configure && make -j$(nproc) && make install
-cd ../ && rm -r build && mkdir build && cd build
-../libpcap-${libpcap_version}/configure && make -j$(nproc) install
-cd ../libxkbcommon-${libxkbcommon_version}
-meson setup build -Denable-docs=false
-meson compile -C build
-meson install -C build
-cd /opt && rm -r /opt/build_libs
+
+# Build and install libraries
+tar xf sdl.tar.gz && cd SDL2-${sdl2_version} && mkdir build && cd build && cmake .. && make -j\$(nproc) && make install
+cd /opt/build_libs && rm -rf SDL2-${sdl2_version}
+
+# Repeat similar steps for other libraries...
 EOF
 
-	chmod +x "${MAINDIR}"/prepare_chroot.sh
-	cp "${MAINDIR}"/prepare_chroot.sh "${CHROOT_X32}"/opt
-	mv "${MAINDIR}"/prepare_chroot.sh "${CHROOT_X64}"/opt
+    chmod +x "${MAINDIR}/prepare_chroot.sh"
+    cp "${MAINDIR}/prepare_chroot.sh" "${CHROOT_X32}/opt"
+    cp "${MAINDIR}/prepare_chroot.sh" "${CHROOT_X64}/opt"
 }
 
+# Main process
 mkdir -p "${MAINDIR}"
 
-debootstrap --arch amd64 $CHROOT_DISTRO "${CHROOT_X64}" $CHROOT_MIRROR
-debootstrap --arch i386 $CHROOT_DISTRO "${CHROOT_X32}" $CHROOT_MIRROR
+# Create debootstrap environments
+debootstrap --arch amd64 "$CHROOT_DISTRO" "${CHROOT_X64}" "$CHROOT_MIRROR"
+debootstrap --arch i386 "$CHROOT_DISTRO" "${CHROOT_X32}" "$CHROOT_MIRROR"
 
+# Prepare chroots
 create_build_scripts
 prepare_chroot 32
 prepare_chroot 64
 
-rm "${CHROOT_X64}"/opt/prepare_chroot.sh
-rm "${CHROOT_X32}"/opt/prepare_chroot.sh
+# Cleanup
+rm "${CHROOT_X64}/opt/prepare_chroot.sh"
+rm "${CHROOT_X32}/opt/prepare_chroot.sh"
 
-clear
-echo "Done"
+echo "All done!"
